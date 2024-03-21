d2620c0578b821d590be80c0b8bd2d5175da4fc4
difelk <dilshan.f@theavo.net>
2024-03-21 06:27:57 +0530

    created stock controllers

--- src/main/java/com/bcn/stockService/controller/StockController.java
+++ src/main/java/com/bcn/stockService/controller/StockController.java
@@ -1,12 +1,24 @@
 package com.bcn.stockService.controller;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class StockController {
 
+    @GetMapping(path = "/stocks")
+    public String getAllStocks() {
+        return "stock called";
+    }
 
+    @PostMapping(path = "/stocks")
+    public String createStock(@RequestBody String stockData) {
+        return "Stock created";
+    }
 
-
+    @PutMapping(path = "/stocks/{id}")
+    public String updateStock(@PathVariable Long id, @RequestBody String stockData) {
+        return "Stock updated";
+    }
 }
+

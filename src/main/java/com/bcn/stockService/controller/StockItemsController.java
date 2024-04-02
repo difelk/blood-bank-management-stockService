cef62135b0dd19f804bf5f66c3a32505dc899339
difelk <dilshan.f@theavo.net>
2024-04-02 22:10:24 +0530

    stock items next steps described

--- src/main/java/com/bcn/stockService/controller/StockItemsController.java
+++ src/main/java/com/bcn/stockService/controller/StockItemsController.java
@@ -17,17 +17,38 @@ public class StockItemsController {
 
     @GetMapping(path = "/stock-items")
     public List<StockItems> getAllStockItems() {
+
+        //do the frontend mapping and show in stock table
+        // stock and the stock items both need to fetch and show in frontend.
+        // no need to do the modals' data. I will do those, once you completed the data mapping.
         return stockItemsService.getAllStockItems();
     }
 
     @GetMapping(path = "/stock-items/{stockDate}")
     public List<StockItems> getStockItemsByDate(@PathVariable Date stockDate) {
+
+//        already implemented this in donor history to check stock already exist
+        //can verify this working. no changes need to be done in here.
+
         return stockItemsService.getStockItemsByDate(stockDate);
     }
 
     @PostMapping(path = "/stock-items")
     public StockItems createStockItems(@RequestBody StockItem stockItem){
         System.out.println("called post stock item controller");
+// I have already fixed the stock item issues here. Check the createStockItems method.
+// I explained the next step.
+// Also, we need to create a stock items PUT endpoint.
+// But there is an issue: when we send blood type, NIC, and quantity to update stock items(from donor history to stock items),
+// we cannot identify which stock item needs to be reduced or incremented.
+// Since we store all the data in each blood type, there is no way to specify or catch the updating quantity.
+// Either we have to create another table called stock item details
+// and put id, NIC, data, blood type, and quantity of that day.
+// If we use this approach, when stock item is called, first we need to update the stock item details table,
+// then the stock item once the stock item details are successful,
+// and finally the stock table.
+//I have already finished the donor history to send data to stock. you can try it with frontend end . but get a latest from
+//all the repos first
         return stockItemsService.createStockItems(stockItem);
     }
 }
--- src/main/java/com/bcn/stockService/service/StockItemsService.java
+++ src/main/java/com/bcn/stockService/service/StockItemsService.java
@@ -33,12 +33,27 @@ public class StockItemsService {
             System.out.println("Failed to find stock items by Date: " + e.getMessage());
             return null;
         }
-    }public StockItems isDateExists(Date stockDate){
+    }
+
+    public StockItems isDateExists(Date stockDate) {
         return stockItemRepository.isDateExists(stockDate);
     }
 
-    public StockItems createStockItems(StockItem stockItem){
-        System.out.println("stockItem in service - " +  stockItem.getDonorNic());
+    public StockItems createStockItems(StockItem stockItem) {
+        System.out.println("stockItem in service - " + stockItem.getDonorNic());
+
+// In this scenario, we have a new stock item, which means this stock item's date does not exist in the stock item table.
+// Therefore, we need to create a new stock item.
+// Before that, since we have data in the stockItem object but the database has data organized in the "stockItems" object,
+// you need to first create a stock items object here and use a switch case to check the stock item blood type (above stockItem parameter donor blood type).
+// Then assign the correct setter method with the blood type quantity and set other blood types as 0.
+// Then you can create the save query by passing the whole newly created stockItems object.
+//once the stock item successful then send the stockItem qty to the stock tb.
+        // for testing I commented out the save method.
+        //and return null because method is expecting return value
+        //also created meaningful respond method for all the requests
+
+
         return null;
 //        return stockItemRepository.save(stockItems);
     }
